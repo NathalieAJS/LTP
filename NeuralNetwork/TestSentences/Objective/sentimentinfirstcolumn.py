3e2fd8c0a1292f4c6e9445fafe16fc0246b7c0b2
NathalieAJS <nathaliestierman@hotmail.com>
2017-06-18 22:17:29 +0200

    Add files via upload

---
+++ NeuralNetwork/TestSentences/Objective/sentimentinfirstcolumn.py
@@ -0,0 +1,29 @@
+# Set sentiment in the first column of all tweets
+# Nathalie Stierman - S2544229
+# python3 positivetweets.py name_of_old_file.txt
+
+import sys
+
+def main(argv):
+	# Open files
+	with open(argv[1], "r") as f:
+		data = f.readlines()
+		f.close()
+	
+	#count = 0
+	file = open("newpositivetweets.txt", "w")
+	for line in data:
+		sentiment = line.split()
+		# if the sentence is marked as positive, place tweet in file
+		if sentiment[4] == 'objective' or 'objective' in sentiment[2]:
+			#print(sentiment[0], "sentiment 0")
+			#sentiment[0] = 'positive'
+			#print(sentiment[0], "sentiment 0 hierna")
+			newline = 'objective'+ ' ' + line
+			print(newline)
+			file.write(newline)
+			#count += 1
+		
+if __name__ == "__main__":
+	if len(sys.argv) == 2:
+		main(sys.argv)
